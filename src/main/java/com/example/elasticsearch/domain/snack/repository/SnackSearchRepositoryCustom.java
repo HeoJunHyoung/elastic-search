07cbf5a36fbc1435ff3bb603d9d97715e344d902
junbro <gjwnsgud4016@gmail.com>
2025-12-12 09:49:30 +0900

    chore

--- src/main/java/com/example/elasticsearch/domain/snack/repository/SnackSearchRepositoryCustom.java
+++
@@ -1,9 +0,0 @@
-package com.example.elasticsearch.domain.snack.repository;
-
-import com.example.elasticsearch.domain.snack.document.SnackDocument;
-
-import java.util.List;
-
-public interface SnackSearchRepositoryCustom {
-    List<SnackDocument> searchByKeyword(String keyword);
-}
